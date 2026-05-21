6c85b62fffe11255858063b188adcdceb7307b3b
William Beh <willbeh@gmail.com>
2026-05-21 19:46:22 +0800

    don't route

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-const hostname = window.location.hostname;
-if (hostname !== 'pokeratwork.com' && hostname !== 'www.pokeratwork.com' && hostname !== 'localhost') {
-  window.location.replace(
-    'https://pokeratwork.com' + window.location.pathname + window.location.search + window.location.hash
-  );
-}
+// const hostname = window.location.hostname;
+// if (hostname !== 'pokeratwork.com' && hostname !== 'www.pokeratwork.com' && hostname !== 'localhost') {
+//   window.location.replace(
+//     'https://pokeratwork.com' + window.location.pathname + window.location.search + window.location.hash
+//   );
+// }
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
